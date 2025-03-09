51893c6be0c662ef2307a8190ca5c62de8f257b2
Mårten Åsberg <marten.asberg@outlook.com>
2025-03-09 16:33:06 +0100

    Custom AR button

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,69 @@
 import "./styles.css";
-import { Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneGeometry, Scene, SRGBColorSpace, TextureLoader, WebGLRenderer } from "three";
-import { ARButton } from "three/examples/jsm/webxr/ARButton";
+import { Camera, Mesh, MeshBasicMaterial, PlaneGeometry, Scene, SRGBColorSpace, TextureLoader, WebGLRenderer } from "three";
 
 window.addEventListener("load", async () => {
-  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
-  const renderer = new WebGLRenderer({ canvas, antialias: true, alpha: true });
+  const isArSupported = await navigator.xr?.isSessionSupported("immersive-ar");
+  if (!isArSupported) {
+    return;
+  }
+
+  const renderer = new WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.xr.enabled = true;
 
-  const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40);
+  const camera = new Camera();
 
   const targetPainting = await window.createImageBitmap(await (await window.fetch("target.png")).blob());
   const texture = await new TextureLoader().loadAsync("replacement.png");
   texture.colorSpace = SRGBColorSpace;
-  const scale = 2;
+  const scale = 1;
 
   const scene = new Scene();
   const targetAspectRatio = targetPainting.width / targetPainting.height;
   const textureAspectRatio = texture.image.width / texture.image.height;
-  console.log({ targetAspectRatio, textureAspectRatio });
   const [width, height] = targetAspectRatio < textureAspectRatio
     ? [1, 1 / textureAspectRatio]
     : [textureAspectRatio / targetAspectRatio, 1 / targetAspectRatio];
   const geometry = new PlaneGeometry(width * scale, height * scale);
   geometry.rotateX(-Math.PI / 2);
-  const replacementPainting = new Mesh(geometry, new MeshBasicMaterial({ map: texture, transparent: true }));
+  const replacementPainting = new Mesh(geometry, new MeshBasicMaterial({ map: texture }));
   replacementPainting.matrixAutoUpdate = false;
   replacementPainting.visible = false;
   scene.add(replacementPainting);
 
-  const arButton = ARButton.createButton(renderer, {
-    requiredFeatures: ["image-tracking"],
-    trackedImages: [
-      {
-        image: targetPainting,
-        widthInMeters: 1,
-      },
-    ],
-  });
-  document.body.appendChild(arButton);
+  document.getElementById("ar-not-supported")!.style.display = "none";
+  const viewInArButton = document.getElementById("view-in-ar") as HTMLButtonElement;
+  viewInArButton.style.display = "block"
 
-  renderer.setAnimationLoop((_, frame) => {
-    replacementPainting.visible = false;
-    if (frame) {
-      for (const imageTracker of frame.getImageTrackingResults()) {
-        const pose = frame.getPose(imageTracker.imageSpace, renderer.xr.getReferenceSpace()!)!;
+  viewInArButton.addEventListener("click", async () => {
+    const arSession = await navigator.xr!.requestSession("immersive-ar", {
+      requiredFeatures: ["image-tracking"],
+      trackedImages: [
+        {
+          image: targetPainting,
+          widthInMeters: 1,
+        },
+      ],
+    });
+    renderer.xr.setReferenceSpaceType("local");
+    await renderer.xr.setSession(arSession);
 
-        if (imageTracker.trackingState == "tracked") {
-          replacementPainting.visible = true;
-          replacementPainting.matrix.fromArray(pose.transform.matrix);
-          break;
+    renderer.setAnimationLoop((_, frame) => {
+      replacementPainting.visible = false;
+      if (frame) {
+        for (const imageTracker of frame.getImageTrackingResults()) {
+          const pose = frame.getPose(imageTracker.imageSpace, renderer.xr.getReferenceSpace()!)!;
+
+          if (imageTracker.trackingState == "tracked") {
+            replacementPainting.visible = true;
+            replacementPainting.matrix.fromArray(pose.transform.matrix);
+            break;
+          }
         }
       }
-    }
 
-    renderer.render(scene, camera);
+      renderer.render(scene, camera);
+    });
   });
 });
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module "three/examples/jsm/webxr/ARButton" {
-  export class ARButton {
-    static createButton(renderer: WebGLRenderer, options?: XRSessionInit);
-  }
-}
-
 interface XRSessionInit {
   trackedImages: any;
 }
